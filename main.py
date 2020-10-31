c7500fbaa4faae08457c0dfb58ab79081688203d
Kile Alkuri <69253692+KileAlkuri@users.noreply.github.com>
2020-10-31 14:05:47 +0100

    Connecting api cog

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def get_prefix(bot, message):
 
 bot = commands.Bot(command_prefix= get_prefix, description="default prefix", case_insensitive=True, intents = discord.Intents.default())
 bot.remove_command('help')
-cogs = ['devstuff', 'prefixes', 'smallcommands', 'rps']
+cogs = ['devstuff', 'prefixes', 'smallcommands', 'rps', 'api']
 
 for cog in cogs:
     bot.load_extension(f"cogs.{cog}")
@@ -103,129 +103,6 @@ async def support(ctx):
     })
     await ctx.send(embed=embed)
 
-@bot.command()
-async def urban(ctx, content):
-    session = aiohttp.ClientSession() 
-    headers = {'Content-Type': 'application/json',
-        'Authorization': 'Bearer 16c6fa735e974848ea8395a4160b8'} 
-    body = {
-        'args': { 'text': content }
-      } 
-    #t 2-3 hours
-    #c Using fAPI
-    
-    async with session.post('https://fapi.wrmsr.io/urban', headers=headers, json=body) as r: 
-        response = await r.json()
-
-    if response == []:
-        return await ctx.send(':x: Not found')
-
-    
-    desc = urbandesc(response)
-    embed = discord.Embed.from_dict({
-            'title': f'Results for **{content}**',
-            'description': desc,
-
-            'color': 0x1400ff
-            })
-    await ctx.send(embed=embed)
-    session.close
-    
-def urbandesc(array):  
-    desc = f'''**__{array[0]["header"]}__**
-**Meaning** \n{array[0]["meaning"]}\n
-**Example** \n{array[0]["example"]}\n'''
-    
-    try:
-        desc = desc + f'''\n**__{array[2]["header"]}__**
-    **Meaning** \n{array[2]["meaning"]}\n
-    **Example** \n{array[2]["example"]}\n\n'''
-    except Exception as e:
-        print('no')
-    try:
-        desc = desc + f'''\n**__{array[3]["header"]}__**
-    **Meaning** \n{array[3]["meaning"]}\n
-    **Example** \n{array[3]["example"]}\n\n'''
-    except Exception as e:
-        print('no')
-
-    return desc
-
-@bot.command()
-async def cmm(ctx, *, content):
-    #c Change my mind!
-    #t Around 1-2 hours
-    session = aiohttp.ClientSession() 
-    headers = {'Content-Type': 'application/json',
-        'Authorization': 'Bearer 16c6fa735e974848ea8395a4160b8'} 
-    body = {
-        'args': { 'text': content }
-      } 
-    
-    async with session.post('https://fapi.wrmsr.io/changemymind', headers=headers, json=body) as r: 
-        image_bytes = await r.read()
-        file = discord.File(io.BytesIO(image_bytes), filename="image.png")
-    await ctx.send(file=file)
-    
-@bot.command()
-async def quote(ctx, quotist: discord.Member, *, content):
-    #t 2 hours
-    #c powered by fAPI
-    light = False
-    compact = False
-    name = ''
-    now = datetime.now()
-    message = content
-    realcolor = quotist.color
-    hours = f"{now:%I}"
-    if int(hours) < 10:
-        hours = hours[1:]
-    if str(quotist.color) == '#000000':
-        realcolor = '#ffffff'
-    else:
-        realcolor = str(quotist.color)
-
-    if content.startswith('-l'):
-        light = True
-        message = content[2:]
-        if realcolor == '#ffffff':
-            realcolor = '#000000'
-    if content.startswith('-c'):
-        compact = True
-        message = content[2:]
-    if message.startswith(' -c'):
-        compact = True
-        message = message[3:]
-    if message.startswith(' -l'):
-        light = True
-        message = message[3:]
-        if realcolor == '#ffffff':
-            realcolor = '#000000'
-
-    if quotist.nick:
-        name = quotist.nick
-    else: 
-        name = quotist.name
-    session = aiohttp.ClientSession() 
-    headers = {'Content-Type': 'application/json',
-        'Authorization': 'Bearer 16c6fa735e974848ea8395a4160b8'} 
-    body = {
-        'args': { 'message': {'content': message},
-        'author': {'color': realcolor,
-        'bot': quotist.bot,
-        'username': str(name),
-        'avatarURL': str(quotist.avatar_url)},
-        'timestamp':  f'Today at {hours}:{now:%M %p}',
-        'light': light,
-        'compact': compact}
-      } 
-    
-    async with session.post('https://fapi.wrmsr.io/quote', headers=headers, json=body) as r: 
-        image_bytes = await r.read()
-        file = discord.File(io.BytesIO(image_bytes), filename="absolutelyreal.png")
-    await ctx.send(file=file)
-
-    
 
 @bot.command()
 async def function(ctx, *, function):
