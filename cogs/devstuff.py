4634e926d4042bdbc273476d0c81ed3d238828cb
Kile Alkuri <69253692+KileAlkuri@users.noreply.github.com>
2020-10-31 01:58:36 +0100

    Creating devstuff cog

--- cogs/devstuff.py
+++ cogs/devstuff.py
@@ -1,1 +1,32 @@
 
+import discord
+from discord.ext import commands
+
+class devstuff(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+
+    @commands.command(aliases=['eval'])
+    async def exec(self, ctx, *, c):
+        if ctx.author.id == 606162661184372736:
+            try:
+                global bot
+                await ctx.channel.send(f'```py\n{eval(c)}```')
+            except Exception as e:
+                await ctx.channel.send(str(e))
+
+    @commands.command()
+    async def source(self, ctx, name):
+        if ctx.author.id == 606162661184372736 or ctx.author.id == 383790610727043085:
+            func = client.get_command(name).callback
+            code = inspect.getsource(func)
+            await ctx.send('```python\n{}```'.format(code.replace('```', '``')))
+
+
+
+def setup(client):
+    client.add_cog(devstuff(client))
