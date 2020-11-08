97d59ada0988dde238728a7195ccb61146fd4885
Kile <69253692+Kile@users.noreply.github.com>
2020-11-08 17:58:35 +0100

    Add k!translate

---
+++ cogs/translate.py
@@ -0,0 +1,49 @@
+import discord
+from discord.ext import commands
+from deep_translator import (GoogleTranslator,
+PonsTranslator,
+LingueeTranslator,
+MyMemoryTranslator,
+YandexTranslator,
+DeepL,
+QCRI,
+single_detection,
+batch_detection)
+
+class translate(commands.Cog):
+
+  def __init__(self, client):
+    client = self.client
+    
+  @commands.command()
+  async def translate(self, ctx, source, target, *,args):
+
+    langs_list = GoogleTranslator.get_supported_languages()
+    
+
+    embed = discord.Embed.from_dict({ 'title': f'Translation',
+    'description': f'```\n{args}```\n`{source}` -> `{target}`\n',
+    'color': 0xc21a1a})
+
+    try:
+        translated = GoogleTranslator(source=source, target=target).translate(text=args)
+        embed.description = embed.description + '\nSucessfully translated by Google Translator:'
+    except Exception as ge:
+        error = f'Google error: {ge}\n'
+        
+            
+        try:
+            embed.description = embed.description + '\nSucessfully translated by MyMemoryTranslator:'
+            translated = MyMemoryTranslator(source=source, target=target).translate(text=args)
+        except Exception as me:
+            error = error + f'MyMemoryTranslator error: {me}\n'
+            return await ctx.send(error)
+            
+    embed.description = embed.description + f'```\n{translated}```'
+    
+    await ctx.send(embed=embed)
+    
+    
+    
+def setup(client):
+  client.add_cog(translate(client))
