a32ebe5bf3e94802d2edc950c7c73df13819c0be
Kile <69253692+Kile@users.noreply.github.com>
2020-11-24 23:16:45 +0000

    Update test.py

--- killua/cogs/test.py
+++ killua/cogs/test.py
@@ -21,21 +21,21 @@ class test(commands.Cog):
                 "Please give me something to evaluate.\n"
             
         )
-    exprs = ctx.args.split('\n')
-    request = {"expr": exprs,
+        exprs = ctx.args.split('\n')
+        request = {"expr": exprs,
                "precision": 14}
-    async with aiohttp.ClientSession() as session:
-        async with session.post(API_ADDR, data=json.dumps(request)) as resp:
-            answer = await resp.json()
-    if "error" not in answer or "result" not in answer:
-        return await ctx.error_reply(
-             "Sorry, could not complete your request.\n"
-             "An unknown error occurred during calculation!"
-         )
-    if answer["error"]:
-         await ctx.reply("The following error occured while calculating:\n`{}`".format(answer["error"]))
-         return
-    await ctx.reply("Result{}:\n```\n{}\n```".format("s" if len(exprs) > 1 else "", "\n".join(answer["result"])))
+        async with aiohttp.ClientSession() as session:
+            async with session.post(API_ADDR, data=json.dumps(request)) as resp:
+                answer = await resp.json()
+        if "error" not in answer or "result" not in answer:
+            return await ctx.error_reply(
+                "Sorry, could not complete your request.\n"
+                "An unknown error occurred during calculation!"
+            )
+        if answer["error"]:
+            await ctx.reply("The following error occured while calculating:\n`{}`".format(answer["error"]))
+            return
+        await ctx.reply("Result{}:\n```\n{}\n```".format("s" if len(exprs) > 1 else "", "\n".join(answer["result"])))
 
 
     @commands.command()
