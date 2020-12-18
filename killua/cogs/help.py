5c940dce17ebb00b307a4f7e94d111f744003217
Kile <69253692+Kile@users.noreply.github.com>
2020-12-18 14:24:46 +0100

    Neighbour complains about music at new location

--- killua/cogs/help.py
+++ killua/cogs/help.py
@@ -5,7 +5,7 @@ import json
 from json import loads
 from pymongo import MongoClient
 from discord.ext import commands
-from functions import custom_cooldown, blcheck
+from killua.functions import custom_cooldown, blcheck
 with open('config.json', 'r') as config_file:
 	config = json.loads(config_file.read())
 
