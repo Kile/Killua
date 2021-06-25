2a7dac901fb3db8a050377d69f3576a6aa498b12
WanderingWizard <86394469+WanderingWizards@users.noreply.github.com>
2021-06-25 11:27:13 -0500

    Update web-scraping.py

--- killua/cogs/web-scraping.py
+++ killua/cogs/web-scraping.py
@@ -34,8 +34,8 @@ Makes the user to be able to go through results
 ''' 
 
 async def pageturn(msg, page:int, book:str, self, ctx, first_time:bool):
-    async with self.client.session.get(f"https://www.goodreads.com/search?q={book}") as response:
-        content = await response.text()
+    response = await self.client.session.get(f"https://www.goodreads.com/search?q={book}")
+    content = await response.text()
 
     p = BeautifulSoup(content.encode(), 'html.parser')
     if first_time is True:
