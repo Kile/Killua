54fef45c9fe5ac4910e5038d0f7188cd47ec025f
Kile <erik.lippert@icloud.com>
2021-07-08 18:13:34 +0200

    Switching to newer topgg lib

--- killua/cogs/votes.py
+++ killua/cogs/votes.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-import dbl
+import topgg
 import json
 with open('config.json', 'r') as config_file:
     config = json.loads(config_file.read())
@@ -10,17 +10,24 @@ class Vote(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.dbl = config["dbl"]
-        self.token = self.dbl["token"] # set this to your DBL token
-        self.dblpy = dbl.DBLClient(self.client, self.token, webhook_path='/dblwebhook', webhook_auth=self.dbl["password"], webhook_port=self.dbl["port"])
+        self.topgg_webhook = topgg.WebhookManager(self.client).dbl_webhook("/dblwebhook", self.dbl["password"])
+        self.topgg_webhook.run(self.dbl["port"])
 
     @commands.Cog.listener()
-    async def on_ready(self):
-        print(dbl.__version__)
+    async def on_dbl_vote(data):
+        """An event that is called whenever someone votes for the bot on Top.gg."""
+        if data["type"] == "test":
+            # this is roughly equivalent to
+            # `return await on_dbl_test(data)` in this case
+            return self.client.dispatch("dbl_test", data)
+
+        print(f"Received a vote:\n{data}")
 
     @commands.Cog.listener()
-    async def on_dbl_vote(self, data):
-        print('Someone voted')
-        print(data)
+    async def on_dbl_test(data):
+        """An event that is called whenever someone tests the webhook system for your bot on Top.gg."""
+        print(f"Received a test vote:\n{data}")
+
 
 Cog = Vote
 
