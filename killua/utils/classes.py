b2f538679b3bb421f2bcd8eb129bd22c4ad6c22c
Kile <69253692+Kile@users.noreply.github.com>
2022-01-27 17:47:33 +0000

    :no_entry: remove doubled jenny event

--- killua/utils/classes.py
+++ killua/utils/classes.py
@@ -569,8 +569,7 @@ class User:
 
     @property
     def is_entitled_to_double_jenny(self) -> bool:
-        return True
-        # return self.is_premium and self.premium_tier in list(PATREON_TIERS.keys())[2:]
+        return self.is_premium and self.premium_tier in list(PATREON_TIERS.keys())[2:]
 
     @all_cards.setter
     def all_cards(self, other):
